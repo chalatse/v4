4aa3ca38606242a9e6b8e86d10908f4b5e8e1814
ndivho <ndivho.makhuvha.coding@outlook.com>
2025-09-30 17:30:18 +0200

    added skelenton structure and config

---
+++ Frontend/Onboarding/src/app/home/landing.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { Landing } from './components/landing/landing';
+
+export const landingRoutes: Route[] = [{ path: '', component: Landing }];
---
+++ Frontend/Onboarding/src/app/shared/services/api.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api<T, U = T> {
+  private apiUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  get<TOverride = T>(endpoint: string): Observable<TOverride> {
+    return this.http.get<TOverride>(`${this.apiUrl}/${endpoint}`, {
+      withCredentials: true,
+    });
+  }
+
+  post<TOverride = T>(
+    endpoint: string,
+    body: U | null = null
+  ): Observable<TOverride> {
+    return this.http.post<TOverride>(`${this.apiUrl}/${endpoint}`, body, {
+      withCredentials: true,
+    });
+  }
+
+  put(endpoint: string, body: U): Observable<T> {
+    return this.http.put<T>(`${this.apiUrl}/${endpoint}`, body);
+  }
+
+  delete<TOverride = T>(endpoint: string): Observable<TOverride> {
+    return this.http.delete<TOverride>(`${this.apiUrl}/${endpoint}`, {
+      withCredentials: true,
+    });
+  }
+}
